6e861d492ed775bad696e363ebaafd16c1625083
Michalis Kaseris <mich.kaseris@gmail.com>
2020-02-28 16:32:31 +0000

    Added __contains__ method

--- priority_queue.py
+++ priority_queue.py
@@ -6,7 +6,13 @@ class PriorityQueue(object):
         self._queue = []
 
     def __str__(self):
+        """Prints the contents of the priority queue. Enables the print function
+        to work on the priority queue."""
         return ' '.join([str(i) for i in self._queue])
+
+    def __contains__(self, key):
+        """Enables the 'in' operator to work."""
+        return key in [key for key, _, _ in self._queue]
 
     def insert(self, item):
         heapq.heappush(self._queue, item)
@@ -17,14 +23,11 @@ class PriorityQueue(object):
     def isEmpty(self):
         return len(self._queue) == 0
 
-
+# Test
 if __name__ == '__main__':
     pq = PriorityQueue()
-    pq.insert(0, 'london')
-    pq.insert(100, 'birmingham')
-    pq.insert(40, 'brighton')
-    pq.insert(50, 'oxford')
-    pq.insert(60, 'cambridge')
-    print(pq)
-    while(not pq.isEmpty()):
-        print(pq.pop())
+    pq.insert(('london', 0, ['london']))
+    node = 'aberdeen'
+    print(node in pq) # False
+    node = 'london'
+    print(node in pq) # True
