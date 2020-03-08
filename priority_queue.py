b7e43d6126933647a2b919f6d898229978fd2d9e
Michalis Kaseris <mich.kaseris@gmail.com>
2020-03-08 00:26:18 +0000

    Additional tests

--- priority_queue.py
+++ priority_queue.py
@@ -1,16 +1,23 @@
 import heapq
-
+# Change this to cost < c
 def isCostHigher(pq, key, cost):
     for c, k, _, _ in pq._queue:
-        if key==k and cost > c:
+        if key==k and cost < c:
             return True
         return False
 
 def replace(pq, item):
+    count = 0
     for i in range(len(pq._queue)):
-        if pq._queue[i][1] == item[1]:
-            del pq._queue[i]
-            pq._queue.append(item)
+        if pq._queue[i][1] == item[1] and item[0] < pq._queue[i][0]:
+            pq._queue[i] = (-1, item[1], item[2], item[3])
+            count += 1
+    heapq.heapify(pq._queue)
+
+    for i in range(count):
+        pq.pop()
+
+    pq.insert(item)
 
 class PriorityQueue(object):
 
@@ -37,23 +44,43 @@ class PriorityQueue(object):
 
 # Test
 if __name__ == '__main__':
-    pq = PriorityQueue()
-    pq.insert((10, 'london', ['london']))
-    node = 'aberdeen'
-    print(node in pq) # False
-    node = 'london'
-    print(node in pq) # True
-    print(pq)
+    # pq = PriorityQueue()
+    # pq.insert((10, 'london', ['london']))
+    # node = 'aberdeen'
+    # print(node in pq) # False
+    # node = 'london'
+    # print(node in pq) # True
+    # print(pq)
+    #
+    # print("\nTesting isCostHigher()")
+    # child_node = 'london'
+    # child_cost = 100
+    # print(isCostHigher(pq, child_node, child_cost)) # True
+    # print("\nTesting replace()...")
+    # replace(pq, (child_cost, child_node, ['brighton', 'london']))
+    # print(pq)
+    #
+    # print("\nTesting the statement")
+    # if isCostHigher(pq, child_node, child_cost):
+    #     replace(pq, (child_cost, child_node, ['brighton', 'london']))
+    # print(pq)
 
-    print("\nTesting isCostHigher()")
-    child_node = 'london'
-    child_cost = 100
-    print(isCostHigher(pq, child_node, child_cost)) # True
-    print("\nTesting replace()...")
-    replace(pq, (child_cost, child_node, ['brighton', 'london']))
-    print(pq)
+    pq2 = PriorityQueue()
+    pq2.insert((5, 'wow', 0.0, []))
+    pq2.insert((4, 'wow', 0.0, []))
+    pq2.insert((32, 'nikos', 0.0, ['asdad']))
+    pq2.insert((7, 'wow', 0.0, []))
+    pq2.insert((8, 'trakis', 0.0, ['asdasd']))
+    pq2.insert((849, 'wow', 0.0, ['asd']))
+    print("Test the replace")
+    item = (2, 'wow', 0.0, ['asdasdas'])
+    child = 'trakis'
+    print("IS IN FRONTIER: {}".format(child in pq2))
+    print("IS COST HIGHER IN FRONTIER: {}".format(isCostHigher(pq2, 'wow', 2)))
+    aDict = {'bekos': 0, 'wow': 2, 'trakis': 20}
+    print("IS THE NODE IN THE DICT? {}".format(child in aDict))
+    if child in pq2 and isCostHigher(pq2, 'wow', 2):
+        replace(pq2, (2 + 1, 'wow', 0.0, ['hey']))
 
-    print("\nTesting the statement")
-    if isCostHigher(pq, child_node, child_cost):
-        replace(pq, (child_cost, child_node, ['brighton', 'london']))
-    print(pq)
+    while not pq2.isEmpty():
+        print(pq2.pop())
